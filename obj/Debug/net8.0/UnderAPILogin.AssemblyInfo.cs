02dd2872ccd25dbce2e151e641fce8d7b319c015
Lucas Fabris <gflucas23@gmail.com>
2024-02-02 22:10:31 -0300

    Criação de regras de e-mail

--- obj/Debug/net8.0/UnderAPILogin.AssemblyInfo.cs
+++ obj/Debug/net8.0/UnderAPILogin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnderAPILogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a30f00477c0455d3d5115881738c6f9ba8c4e0d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+debdcbedd43d94b0cc51726e63dc2708be29fa8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnderAPILogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnderAPILogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
